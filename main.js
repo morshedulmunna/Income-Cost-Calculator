02dc41b8ef784b3f9104e0c4054d45eacd9ad275
morshedulmunna <morshedulmunna1@gmail.com>
2022-02-17 04:41:13 +0600

    saveing ammount done

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const income = document.getElementById("income");
 const food = document.getElementById("food");
 const rent = document.getElementById("rent");
 const cloth = document.getElementById("cloth");
+const save = document.getElementById("save");
+const savingAccount = document.getElementById("savingAccount");
 const totalExpenses = document.getElementById("totalExpenses");
 const balance = document.getElementById("balance");
 
@@ -13,16 +15,16 @@ function calculate() {
   let totalCost = foodCost + rentCost + clothCost;
 
   //==error Handleing to Empty Input
-  if (
+  /*  if (
     (isNaN(incomeBlance) || incomeBlance < 0,
     isNaN(foodCost) || foodCost < 0,
     isNaN(rentCost) || rentCost < 0,
     isNaN(clothCost) || clothCost < 0)
   ) {
-    alert("Please enter Number");
+    alert("Please enter Positive Number Only");
     inputEmpty();
     return false;
-  }
+  } */
   //==Calculate total Expenses Cost
   totalExpenses.innerText = totalCost;
 
@@ -36,7 +38,22 @@ function inputEmpty() {
   food.value = "";
   rent.value = "";
   cloth.value = "";
-  income.value = "";
   totalExpensesValue = "0";
   dueBalance = "0";
+}
+income.addEventListener("focus", () => {
+  income.value = "";
+});
+
+//===Saveing balance calculate
+function saveAmount() {
+  let incomeBlance = parseFloat(income.value);
+  let savePersentage = parseInt(save.value);
+
+  //==error Handle
+
+  percentageResult = savePersentage / 100;
+  result = incomeBlance * percentageResult;
+  saveings = result.toFixed(2);
+  savingAccount.innerText = saveings;
 }
