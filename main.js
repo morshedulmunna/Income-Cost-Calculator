372b7c7fa375c1685662c848e04cd28f65980d8e
morshedulmunna <morshedulmunna1@gmail.com>
2022-02-17 03:19:39 +0600

    error handle in calculator btn

--- main.js
+++ main.js
@@ -6,20 +6,37 @@ const totalExpenses = document.getElementById("totalExpenses");
 const balance = document.getElementById("balance");
 
 function calculate() {
-  //==Calculate total Expenses Cost
+  let incomeBlance = parseFloat(income.value);
   let foodCost = parseFloat(food.value);
   let rentCost = parseFloat(rent.value);
   let clothCost = parseFloat(cloth.value);
-  let total = foodCost + rentCost + clothCost;
-  let totalExpensesValue = parseFloat(totalExpenses.innerText);
-  totalExpenses.innerText = total + totalExpensesValue;
+  let totalCost = foodCost + rentCost + clothCost;
+
+  //==error Handleing to Empty Input
+  if (
+    (isNaN(incomeBlance) || incomeBlance < 0,
+    isNaN(foodCost) || foodCost < 0,
+    isNaN(rentCost) || rentCost < 0,
+    isNaN(clothCost) || clothCost < 0)
+  ) {
+    alert("Please enter Number");
+    inputEmpty();
+    return false;
+  }
+  //==Calculate total Expenses Cost
+  totalExpenses.innerText = totalCost;
 
   //==total Balance after expenses
-  let incomeBlance = parseFloat(income.value);
-  let dueBalance = parseFloat(balance.innerText);
-  balance.innerText = dueBalance + (incomeBlance - total);
-  //==set value Empty after Click
+  balance.innerText = incomeBlance - totalCost;
+  inputEmpty();
+}
+
+//==set value Empty after Click
+function inputEmpty() {
   food.value = "";
   rent.value = "";
   cloth.value = "";
+  income.value = "";
+  totalExpensesValue = "0";
+  dueBalance = "0";
 }
