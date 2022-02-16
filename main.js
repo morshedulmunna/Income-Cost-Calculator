add148cd82e9649df4b4b660964f7eca8e97bbf7
morshedulmunna <morshedulmunna1@gmail.com>
2022-02-17 02:36:30 +0600

    expense calculation

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello Log");
+const income = document.getElementById("income");
+const food = document.getElementById("food");
+const rent = document.getElementById("rent");
+const cloth = document.getElementById("cloth");
+const totalExpenses = document.getElementById("totalExpenses");
+const balance = document.getElementById("balance");
+
+function calculate() {
+  //==Calculate total Expenses Cost
+  let foodCost = parseFloat(food.value);
+  let rentCost = parseFloat(rent.value);
+  let clothCost = parseFloat(cloth.value);
+  let total = foodCost + rentCost + clothCost;
+  let totalExpensesValue = parseFloat(totalExpenses.innerText);
+  totalExpenses.innerText = total + totalExpensesValue;
+
+  //==total Balance after expenses
+  let incomeBlance = parseFloat(income.value);
+  let dueBalance = parseFloat(balance.innerText);
+  balance.innerText = dueBalance + (incomeBlance - total);
+  //==set value Empty after Click
+  food.value = "";
+  rent.value = "";
+  cloth.value = "";
+}
