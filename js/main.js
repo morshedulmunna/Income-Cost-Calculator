d42c18b9e3b52440e8addece2fae529c708c78c9
morshedulmunna <morshedulmunna1@gmail.com>
2022-02-17 16:55:38 +0600

    function customize and duplicate remove

--- js/main.js
+++ js/main.js
@@ -15,17 +15,25 @@ function calculate() {
   let clothCost = parseFloat(cloth.value);
   let totalCost = foodCost + rentCost + clothCost;
 
-  // Error Handle
-  errorHandle();
-  //==Calculate total Expenses Cost
-  totalExpenses.innerText = totalCost.toFixed(2);
+  errorCheck = errorHandle(
+    incomeBlance,
+    foodCost,
+    rentCost,
+    clothCost,
+    totalCost
+  );
+  if (errorCheck == true) {
+    //==Calculate total Expenses Cost
+    totalExpenses.innerText = totalCost.toFixed(2);
 
-  //==total Balance after expenses
-  let balanceResult = incomeBlance - totalCost;
-  balance.innerText = balanceResult.toFixed(2);
+    //==total Balance after expenses
+    let balanceResult = incomeBlance - totalCost;
+    balance.innerText = balanceResult.toFixed(2);
+  }
 }
+//
 
-//==When Focused Input Box then Input Value is Empty Event
+//==When Focused Input Box then Input Value is Empty Event Handler
 income.addEventListener("focus", () => {
   income.value = "";
   valueEmpty();
@@ -54,7 +62,11 @@ function valueEmpty() {
   savingAccount.innerText = "0";
   remainingBalance.innerText = "0";
 }
-//===Saveing balance & Remaining Balance calculate
+
+//
+
+//
+//===Saveing balance & Remaining Balance calculation
 function saveAmount() {
   let incomeBlance = parseFloat(income.value);
   let savePersentage = parseInt(save.value);
@@ -63,33 +75,48 @@ function saveAmount() {
   percentageResult = savePersentage / 100;
   result = incomeBlance * percentageResult;
   saveings = result.toFixed(2);
-  savingAccount.innerText = saveings;
   remainingTotalBalance = duebalance - saveings;
-  remainingBalance.innerText = remainingTotalBalance.toFixed(2);
-  //==error Handle
-  if (duebalance < saveings) {
-    alert("Your balance not Enough to Save");
-  } else if (savePersentage < 0) {
-    alert("You can't input Negative Number");
+
+  errorCheck = saveError(duebalance, saveings, savePersentage);
+  if (errorCheck == true) {
+    savingAccount.innerText = saveings;
+    remainingBalance.innerText = remainingTotalBalance.toFixed(2);
   }
 }
 
-function errorHandle() {
-  let incomeBlance = parseFloat(income.value);
-  let foodCost = parseFloat(food.value);
-  let rentCost = parseFloat(rent.value);
-  let clothCost = parseFloat(cloth.value);
-  let totalCost = foodCost + rentCost + clothCost;
+//
+
+// Income Expenses calculation Error Hander Checking function
+function errorHandle(incomeBlance, foodCost, rentCost, clothCost, totalCost) {
   //==error Handleing to Empty Input
   if (incomeBlance < 0 || isNaN(incomeBlance)) {
     alert("Please Inter only Positive Number");
+    return false;
   } else if (foodCost < 0 || isNaN(foodCost)) {
     alert("Please Inter only Positive Number");
+    return false;
   } else if (rentCost < 0 || isNaN(rentCost)) {
     alert("Please Inter only Positive Number");
+    return false;
   } else if (clothCost < 0 || isNaN(clothCost)) {
     alert("Please Inter only Positive Number");
+    return false;
   } else if (incomeBlance < totalCost) {
     alert("Your Income Amount not Enough");
+    return false;
   }
+  return true;
+}
+
+// Save Ammout And remaing Ammout Error Handle Function
+function saveError(duebalance, saveings, savePersentage) {
+  //==error Handle
+  if (duebalance < saveings) {
+    alert("Your balance not Enough to Save");
+    return false;
+  } else if (savePersentage < 0 || isNaN(savePersentage)) {
+    alert("You can't input Negative Number or String ");
+    return false;
+  }
+  return true;
 }
